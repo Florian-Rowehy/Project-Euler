5eb71eb2621d392d797da522666d1402b413a815
Florian-Rowehy <florian.rowehy@gmail.com>
2021-04-11 19:05:25 +0200

    updated solution 013 to 015, added description

--- 014_longest_collatz_sequence.js
+++ 014_longest_collatz_sequence.js
@@ -1,22 +1,42 @@
+/*
+  The following iterative sequence is defined for the set of positive integers:
+
+  n → n/2 (n is even)
+  n → 3n + 1 (n is odd)
+  Using the rule above and starting with 13, we generate the following sequence:
+
+  13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+  It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+
+  Which starting number, under the given limit, produces the longest chain?
+
+  Note: Once the chain starts the terms are allowed to go above one million.
+*/
+
 function longestCollatzSequence(limit) {
+  //since even numbers start by a division, only take uneven number
   const start = limit % 2 === 0 ? limit-1 : limit;
-  let max = [0, ""];
+  let maxSequenceLength = 0;
+  let nb = limit;
   for(let i = start; i>limit/2; i-=2){
-    const chainLengthI = collatzCount(i);
-    if(max[0] < chainLengthI) max = [chainLengthI, i];
+    const sequenceLength = collatzCount(i);
+
+    if (maxSequenceLength < sequenceLength) {
+      maxSequenceLength = sequenceLength;
+      nb = i;
+    }
   }
-  return max[1];
+  return nb;
 }
 
 function collatzCount(n) {
-  let count = 1;
+  let count = 0;
   while (n > 1) {
+    count++;
     if (n % 2 === 0) {
       n = n / 2;
-      count++;
     } else {
       n = 3 * n + 1;
-      count++;
     }
   }
   return count
--- 015_lattice_paths.js
+++ 015_lattice_paths.js
@@ -1,3 +1,10 @@
+/*
+  Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+
+  a diagram of 6 2 by 2 grids showing all the routes to the bottom right corner
+  How many such routes are there through a given gridSize?
+*/
+
 function latticePaths(gridSize) {
   //array initialisation;
   const arr = [];
